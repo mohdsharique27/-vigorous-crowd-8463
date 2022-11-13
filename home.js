e758d279ef678bef6aa65c97df9d88c018ef54a2
mohdsharique27 <shariqueghazi2427@gmail.com>
2022-11-13 19:49:58 +0530

    new API is applied

--- home.js
+++ home.js
@@ -40,75 +40,61 @@ function showSlide(){
     setTimeout(showSlide,2000);
 }
 
-let bag={};
-let url = "https://dummyjson.com/products"
+let bag = []
+let cartItems=JSON.parse(localStorage.getItem("cartProducts"))
+let url ="https://fakestoreapi.com/products";
 fetch(url)
 .then((res) => res.json())
-.then((data) => {
-    bag= data.products;
-    showProducts(bag)
-    // console.log(bag)
-})
+.then((data) =>{
+    bag = data;
+    console.log(data)
 
-function showProducts(bag){
-bag.forEach((elem) => {
-    let currImage = 0;
-
-    function updateImage(){
-        ProImage.setAttribute("src",elem.images[currImage])
-    }
-    let div = document.createElement("div");
-    
-    let ProImage = document.createElement("img");
-    ProImage.setAttribute("src",elem.images[currImage])
-    ProImage.setAttribute("height",200)
-    ProImage.setAttribute("width",300)
-
-    let ProName = document.createElement("h2");
-    ProName.innerText=elem.title
-
-    let ProPrice = document.createElement("h3");
-    ProPrice.innerHTML= " <strong>₹</strong>"+elem.price;
-
-    let ProCategory = document.createElement("p");
-    ProCategory.innerText=elem.category;
-    
-    let cart_btn= document.createElement("button");
-    cart_btn.innerText="Add to Cart";
-
-    let nextButton = document.createElement("button");
-    let prevButton = document.createElement("button");
-
-    nextButton.innerText = "Next";
-    prevButton.innerText = "Previous";
-    nextButton.addEventListener("click",()=>{
-        if(currImage>elem.images.length-1) {
-            return
-        }
-        currImage++;
-        updateImage()
-    })
-
-    div.append(ProImage,ProName,ProPrice,ProCategory,cart_btn,nextButton,prevButton);
-    
-    document.querySelector(".trendingProducts").append(div);
+    displayCards(data);
 });
- }
 
-function handleSort(){
-   let selected = document.querySelector("#filter").value
-   if(selected=="HTL"){
-    // DECENDING
-    bag.products.sort((a,b)=>b.bag.price-a.bag.price)
-   }
-   if(selected=="LTH"){
-    // ACCENDING
-    bag.sort((a,b)=>a.bag.price-b.bag.price)
+//Search 
+
+function search(){
+    let q= document.querySelector("input").value
     
-   }
+     let newData = bag.filter(function(elem){
+      return elem.title.toLowerCase().includes(q.toLowerCase())||[];
+      
+     });
+     displayCards(newData);
+     
+  }
 
-   showProducts(bag)
+
+
+function displayCards(data){
+ data.forEach(function (elem){
+    let div = document.createElement("div");
+
+    let image = document.createElement("img");
+    image.setAttribute("src",elem.image)
+    let title = document.createElement("h2");
+    title.innerText = elem.title
+    let cost = document.createElement("p")
+    cost.innerText = elem.price
+
+    let rating = document.createElement("p")
+    rating.innerText = elem.rating.rate
+
+    let cart_btn = document.createElement("button");
+        cart_btn.innerText="Add to Cart";
+
+        cart_btn.addEventListener("click",function(){
+            cartItems.push(elem)
+            localStorage.setItem("cartProducts",JSON.stringify(cartItems))
+        })
+
+    div.append(image,title,cost,rating,cart_btn)
+    document.querySelector(".trending-container").append(div);
+ });
+    
 }
+
 
 
 
